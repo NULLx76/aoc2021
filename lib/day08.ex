72181010c86d7f3df7fc9523e725be3d819944ea
Victor Roest <victor@xirion.net>
2021-12-09 15:40:10 +0100

    day 08: final cleanup

--- lib/day08.ex
+++ lib/day08.ex
@@ -5,30 +5,32 @@ defmodule Aoc2021.Day08 do
     File.read!(file)
     |> String.split("\n", trim: true)
     |> Enum.map(fn line ->
-      String.split(line, "|", trim: true)
-      |> Enum.map(&String.split(&1, " ", trim: true))
+      String.split(line, "|")
+      |> Enum.map(fn part ->
+        String.split(part)
+        |> Enum.map(&String.to_charlist/1)
+       end)
       |> List.to_tuple()
     end)
   end
 
-  def is_1478?(chars), do: String.length(chars) in [2, 4, 3, 7]
+  defp is_1478?(chars), do: length(chars) in [2, 4, 3, 7]
 
   def part1(file \\ "./inputs/day08.txt") do
     parse(file)
-    |> Enum.map(&elem(&1, 1))
-    |> Enum.map(&Enum.filter(&1, fn e -> is_1478?(e) end))
-    |> Enum.map(&Enum.count/1)
-    |> Enum.sum()
+    |> Enum.flat_map(&elem(&1, 1))
+    |> Enum.filter(&is_1478?/1)
+    |> Enum.count()
   end
 
   def deduce_number(code, _) when length(code) == 2, do: 1
-  def deduce_number(code, _) when length(code) == 3, do: 7
   def deduce_number(code, _) when length(code) == 4, do: 4
+  def deduce_number(code, _) when length(code) == 3, do: 7
   def deduce_number(code, _) when length(code) == 7, do: 8
 
   def deduce_number(chars, input) do
-    one = Enum.find(input, &(String.length(&1) == 2)) |> String.to_charlist()
-    four = Enum.find(input, &(String.length(&1) == 4)) |> String.to_charlist()
+    one = Enum.find(input, &(length(&1) == 2))
+    four = Enum.find(input, &(length(&1) == 4))
 
     case length(chars) do
       5 ->
@@ -49,7 +51,6 @@ defmodule Aoc2021.Day08 do
 
   def process_line({input, output}) do
     output
-    |> Enum.map(&String.to_charlist/1)
     |> Enum.map(&deduce_number(&1, input))
     |> Integer.undigits()
   end
