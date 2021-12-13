e6d872b572e9386922238ac53d3c47e622b7dc99
Victor Roest <victor@xirion.net>
2021-12-14 00:16:42 +0100

    misc: add benchmark

--- lib/aoc2021.ex
+++ lib/aoc2021.ex
@@ -2,4 +2,11 @@ defmodule Aoc2021 do
   @moduledoc """
   My 2021 Advent of Code solutions
   """
+
+  def benchmark(function) do
+    function
+    |> :timer.tc()
+    |> elem(0)
+    |> then(&(&1 / 1000))
+  end
 end
---
+++ lib/day14.ex
@@ -0,0 +1,15 @@
+defmodule Aoc2021.Day14 do
+  @moduledoc false
+
+  defp parse(file) do
+    File.read!(file)
+  end
+
+  def part1(file \\ "./inputs/day14.txt") do
+    parse(file)
+  end
+
+  def part2(file \\ "./inputs/day14.txt") do
+    parse(file)
+  end
+end
